ec0edcf8e6dd6bade78ead1840769c96c2606a46
Anthony Parrino <anthony21parrino21@gmail.com>
2021-02-27 12:58:46 -0500

    adfa

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -2,8 +2,6 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-require("dotenv").config();
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
